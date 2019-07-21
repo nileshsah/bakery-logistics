ea49269a3d4d175660b6bcacc4e5fbaea71a31b7
Nilesh Sah <nilesh.sah13@gmail.com>
2019-07-21 23:51:47 +0200

    Extend OrderResponse to support total cost and currency

--- src/main/java/com/bakery/logistics/domain/model/OrderResponse.java
+++ src/main/java/com/bakery/logistics/domain/model/OrderResponse.java
@@ -1,5 +1,6 @@
 package com.bakery.logistics.domain.model;
 
+import com.bakery.logistics.domain.exception.CurrencyMismatchException;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
@@ -10,4 +11,29 @@ import java.util.Map;
 public class OrderResponse {
   private Product product;
   private Map<Pack, Integer> packQuantity;
+
+  public Double getTotalCost() {
+    Long currencyCount = packQuantity.entrySet()
+        .stream()
+        .map(keyValue -> keyValue.getKey().getCurrency())
+        .distinct()
+        .count();
+
+    if (currencyCount > 1) {
+      throw new CurrencyMismatchException("More than one currency present for product");
+    }
+
+    return packQuantity.entrySet()
+        .stream()
+        .mapToDouble(keyValue -> keyValue.getKey().getPrice() * keyValue.getValue())
+        .sum();
+  }
+
+  public CurrencyCode getCurrencyCode() {
+    return packQuantity.entrySet()
+        .stream()
+        .findFirst()
+        .map(keyValue -> keyValue.getKey().getCurrency())
+        .orElseThrow(() -> new IllegalStateException("No currency found"));
+  }
 }
